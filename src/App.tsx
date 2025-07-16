b253d12ac095c69a2a6488b9409e2774aa85668f
Manan <manan.zchopra@gmail.com>
2025-07-16 10:48:08 -0700

    All headers now stick to screen

--- src/App.tsx
+++ src/App.tsx
@@ -23,62 +23,65 @@ const App = () => {
 
   return (
     <main className={styles.main}>
-      <div className={styles.iconBarLeft}>
-        <div title="Arch Collaborative">
-          <ArchLogo
-            className={styles.archLogo}
-            onClick={() => {
-              window.open("https://www.arch-collaborative.org/", "_blank");
-            }}
-          />
-        </div>
-        <div
-          title="Keck Human Rights Clinic"
-          onClick={() => {
-            window.open("https://sites.usc.edu/keckhumanrights/home/");
-          }}
-        >
-          <img src={USCLogo} className={styles.uscLogo} />
+      <div className={styles.sticky}>
+        <div className={styles.stickyBar}>
+          <div className={styles.iconBarLeft}>
+            <div title="Arch Collaborative">
+              <ArchLogo
+                className={styles.archLogo}
+                onClick={() => {
+                  window.open("https://www.arch-collaborative.org/", "_blank");
+                }}
+              />
+            </div>
+            <div
+              title="Keck Human Rights Clinic"
+              onClick={() => {
+                window.open("https://sites.usc.edu/keckhumanrights/home/");
+              }}
+            >
+              <img src={USCLogo} className={styles.uscLogo} />
+            </div>
+          </div>
+          <div className={styles.iconBarRight}>
+            <FaRegStar
+              onClick={() => setFavorite(true)}
+              size={40}
+              title={"View selected resources"}
+              style={{ cursor: "pointer" }}
+            />
+            <FiPrinter
+              onClick={() => setPrinter(true)}
+              size={40}
+              title={"Print selected resources"}
+              style={{ cursor: "pointer" }}
+            />
+            <FiTable
+              onClick={() => {
+                window.open(
+                  "https://docs.google.com/spreadsheets/d/1Bpe3AxJnxzBAjRTzWEMYaja1BqaMD1a5EiymTQ_EoHw/edit?usp=sharing",
+                  "_blank"
+                );
+              }}
+              size={40}
+              title="Raw database"
+              style={{ cursor: "pointer" }}
+            />
+            <FaRegQuestionCircle
+              onClick={() => setHelp(true)}
+              size={40}
+              title="Help"
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+          {help && <Info setHelp={setHelp} />}
+          {favorite && <SelectedResourcesView setFavorite={setFavorite} />}
+          {printer && <Printer setPrinter={setPrinter} />}
+          <span className={styles.title}>
+            Los Angeles Social Resources for Patients
+          </span>
         </div>
       </div>
-      <div className={styles.iconBarRight}>
-        <FaRegStar
-          onClick={() => setFavorite(true)}
-          size={40}
-          title={"View selected resources"}
-          style={{ cursor: "pointer" }}
-        />
-        <FiPrinter
-          onClick={() => setPrinter(true)}
-          size={40}
-          title={"Print selected resources"}
-          style={{ cursor: "pointer" }}
-        />
-        <FiTable
-          onClick={() => {
-            window.open(
-              "https://docs.google.com/spreadsheets/d/1Bpe3AxJnxzBAjRTzWEMYaja1BqaMD1a5EiymTQ_EoHw/edit?usp=sharing",
-              "_blank"
-            );
-          }}
-          size={40}
-          title="Raw database"
-          style={{ cursor: "pointer" }}
-        />
-        <FaRegQuestionCircle
-          onClick={() => setHelp(true)}
-          size={40}
-          title="Help"
-          style={{ cursor: "pointer" }}
-        />
-      </div>
-      {help && <Info setHelp={setHelp} />}
-      {favorite && <SelectedResourcesView setFavorite={setFavorite} />}
-      {printer && <Printer setPrinter={setPrinter} />}
-      <span className={styles.title}>
-        Los Angeles Social Resources for Patients
-      </span>
-      
       <ResourceSearch />
     </main>
   );
